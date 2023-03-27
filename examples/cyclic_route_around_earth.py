29e956369fb173706fd3f914a49aba84afd960cb
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2023-03-27 15:39:03 +0200

    formatted examples

--- examples/cyclic_route_around_earth.py
+++ examples/cyclic_route_around_earth.py
@@ -1,32 +1,27 @@
 import geopandas as gpd
+import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
-import matplotlib.pyplot as plt
-import time
 
 from polytope.datacube.xarray import XArrayDatacube
 from polytope.engine.hullslicer import HullSlicer
-from polytope.shapes import *
-from polytope.polytope import Request, Polytope
+from polytope.polytope import Polytope, Request
+from polytope.shapes import Box, PathSegment
 
 
 class Test():
 
     def setup_method(self, method):
         array = xr.open_dataset("./examples/data/output8.grib", engine='cfgrib')
-        options= {"longitude":{"Cyclic":[0,360.]}}
+        options = {"longitude" : {"Cyclic" : [0, 360.]}}
         self.xarraydatacube = XArrayDatacube(array)
         self.slicer = HullSlicer()
-        self.API = Polytope(datacube=array, engine=self.slicer, options = options)
+        self.API = Polytope(datacube=array, engine=self.slicer, options=options)
 
     def test_slice_country(self):
-    
-
-        bounding_box = Box(["latitude", "longitude"], [-0.125,-0.125], [0.125,0.125])
+        bounding_box = Box(["latitude", "longitude"], [-0.125, -0.125], [0.125, 0.125])
         request_obj = PathSegment(["latitude", "longitude"], bounding_box, [-88, -719], [88, 720])
         request = Request(request_obj)
-
-
 
 # Extract the values of the long and lat from the tree
         result = self.API.retrieve(request)
@@ -40,14 +35,14 @@ class Test():
             lat = cubepath["latitude"]
             long = cubepath["longitude"]
             # here need to remap the longitude to be between -180 and 180 so that they appear on the map...
-            if long<-180:
+            if long < -180:
                 long = long + 360
-            if -180<=long<180:
+            if -180 <= long < 180:
                 long = long
-            if 180 <= long<180+360:
+            if 180 <= long < 180+360:
                 long = long-360
             if 180 + 360 <= long < 180 + 360 + 360:
-                long = long -720
+                long = long - 720
             latlong_point = [lat, long]
             lats.append(lat)
             longs.append(long)
@@ -55,82 +50,12 @@ class Test():
             t = t_idx
             temps.append(t)
             country_points_plotting.append(latlong_point)
-        
         temps = np.array(temps)
-
 
 # Plot all the points on a world map
         worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
         fig, ax = plt.subplots(figsize=(12, 6))
         worldmap.plot(color="darkgrey", ax=ax)
-        plt.scatter(longs, lats, s= 8, c=temps, cmap="YlOrRd")
-        plt.colorbar(label="Temperature")
-        plt.show()
-import geopandas as gpd
-import numpy as np
-import xarray as xr
-import matplotlib.pyplot as plt
-import time
-
-from polytope.datacube.xarray import XArrayDatacube
-from polytope.engine.hullslicer import HullSlicer
-from polytope.shapes import *
-from polytope.polytope import Request, Polytope
-
-
-class Test():
-
-    def setup_method(self, method):
-        array = xr.open_dataset("./examples/data/output8.grib", engine='cfgrib')
-        options= {"longitude":{"Cyclic":[0,360.]}}
-        self.xarraydatacube = XArrayDatacube(array)
-        self.slicer = HullSlicer()
-        self.API = Polytope(datacube=array, engine=self.slicer, options = options)
-
-    def test_slice_country(self):
-    
-
-        bounding_box = Box(["latitude", "longitude"], [-0.125,-0.125], [0.125,0.125])
-        request_obj = PathSegment(["latitude", "longitude"], bounding_box, [-88, -719], [88, 720])
-        request = Request(request_obj)
-
-
-
-# Extract the values of the long and lat from the tree
-        result = self.API.retrieve(request)
-        result.pprint()
-        country_points_plotting = []
-        lats = []
-        longs = []
-        temps = []
-        for i in range(len(result.leaves)):
-            cubepath = result.leaves[i].flatten()
-            lat = cubepath["latitude"]
-            long = cubepath["longitude"]
-            # here need to remap the longitude to be between -180 and 180 so that they appear on the map...
-            if long<-180:
-                long = long + 360
-            if -180<=long<180:
-                long = long
-            if 180 <= long<180+360:
-                long = long-360
-            if 180 + 360 <= long < 180 + 360 + 360:
-                long = long -720
-            latlong_point = [lat, long]
-            lats.append(lat)
-            longs.append(long)
-            t_idx = result.leaves[i].result["t2m"]
-            t = t_idx
-            temps.append(t)
-            country_points_plotting.append(latlong_point)
-        
-        temps = np.array(temps)
-
-
-# Plot all the points on a world map
-        worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-        fig, ax = plt.subplots(figsize=(12, 6))
-        worldmap.plot(color="darkgrey", ax=ax)
-        plt.scatter(longs, lats, s= 8, c=temps, cmap="YlOrRd")
+        plt.scatter(longs, lats, s=8, c=temps, cmap="YlOrRd")
         plt.colorbar(label="Temperature")
         plt.show()
--- examples/wind_farms.py
+++ examples/wind_farms.py
@@ -1,15 +1,17 @@
+import math
+
 import geopandas as gpd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+from osgeo import gdal
 from shapely.geometry import shape
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 
 from polytope.datacube.xarray import XArrayDatacube
 from polytope.engine.hullslicer import HullSlicer
-from polytope.shapes import *
-from polytope.polytope import Request, Polytope
-from osgeo import gdal
+from polytope.polytope import Polytope, Request
+from polytope.shapes import Polygon, Select, Union
 
 
 class Test():
@@ -17,12 +19,11 @@ class Test():
     def setup_method(self):
         array = xr.open_dataset("./examples/data/winds.grib", engine='cfgrib')
         self.array = array
-        options= {"longitude":{"Cyclic":[0,360.]}}
+        options = {"longitude" : {"Cyclic" : [0, 360.]}}
         self.xarraydatacube = XArrayDatacube(array)
         self.slicer = HullSlicer()
-        self.API = Polytope(datacube=array, engine=self.slicer, options = options)
+        self.API = Polytope(datacube=array, engine=self.slicer, options=options)
 
-    
     def test_slice_shipping_route(self):
         gdal.SetConfigOption('SHAPE_RESTORE_SHX', 'YES')
 
@@ -38,57 +39,49 @@ class Test():
             else:
                 polygons.append(multi_polygon)
         polygons_list = []
-        
+
         # Now create a list of x,y points for each polygon
 
         for polygon in polygons:
-             xx, yy = polygon.exterior.coords.xy
-             polygon_points = [list(a) for a in zip(xx,yy)]
-             polygons_list.append(polygon_points)
+            xx, yy = polygon.exterior.coords.xy
+            polygon_points = [list(a) for a in zip(xx, yy)]
+            polygons_list.append(polygon_points)
 
         # Then do union of the polygon objects and cut using the slicer
-        print("done")
         poly = []
         for points in polygons_list:
             polygon = Polygon(["longitude", "latitude"], points)
             poly.append(polygon)
-        print(len(poly))
         request_obj = poly[0]
-
         for obj in poly:
             request_obj = Union(["longitude", "latitude"], request_obj, obj)
-    
-        request = Request(request_obj, 
-                Select("step", [np.timedelta64(0, "ns")]))
-
+        request = Request(request_obj,
+                          Select("step", [np.timedelta64(0, "ns")]))
 
         # Extract the values of the long and lat from the tree
         result = self.API.retrieve(request)
-        country_points_plotting = []
         lats = []
         longs = []
         parameter_values = []
         winds_u = []
         winds_v = []
-        array = self.array
         for i in range(len(result.leaves)):
-                cubepath = result.leaves[i].flatten()
-                lat = cubepath["latitude"]
-                long = cubepath["longitude"]
-                if long<-180:
-                    long = long +360
-                if long>180:
-                    long = long -360
-                lats.append(lat)
-                longs.append(long)
-            
-                u10_idx = result.leaves[i].result["u10"]
-                wind_u = u10_idx
-                v10_idx = result.leaves[i].result["v10"]
-                wind_v = v10_idx
-                winds_u.append(wind_u)
-                winds_v.append(wind_v)
-                parameter_values.append(math.sqrt(wind_u**2 + wind_v**2))
+            cubepath = result.leaves[i].flatten()
+            lat = cubepath["latitude"]
+            long = cubepath["longitude"]
+            if long < -180:
+                long = long + 360
+            if long > 180:
+                long = long - 360
+            lats.append(lat)
+            longs.append(long)
+            u10_idx = result.leaves[i].result["u10"]
+            wind_u = u10_idx
+            v10_idx = result.leaves[i].result["v10"]
+            wind_v = v10_idx
+            winds_u.append(wind_u)
+            winds_v.append(wind_v)
+            parameter_values.append(math.sqrt(wind_u**2 + wind_v**2))
 
         parameter_values = np.array(parameter_values)
         # Plot this last array according to different colors for the result on a world map
@@ -96,117 +89,10 @@ class Test():
         fig, ax = plt.subplots(figsize=(12, 6))
         worldmap.plot(color="darkgrey", ax=ax)
         ax.scatter(longs, lats, s=8, c=parameter_values, cmap="viridis")
-        norm = mpl.colors.Normalize(vmin=min(parameter_values),vmax=max(parameter_values))
+        norm = mpl.colors.Normalize(vmin=min(parameter_values), vmax=max(parameter_values))
 
         sm = plt.cm.ScalarMappable(cmap="viridis", norm=norm)
         sm.set_array([])
         plt.colorbar(sm, label="Wind Speed")
 
         plt.show()
-        
-import geopandas as gpd
-import numpy as np
-import xarray as xr
-from shapely.geometry import shape
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-
-from polytope.datacube.xarray import XArrayDatacube
-from polytope.engine.hullslicer import HullSlicer
-from polytope.shapes import *
-from polytope.polytope import Request, Polytope
-from osgeo import gdal
-
-
-class Test():
-
-    def setup_method(self):
-        array = xr.open_dataset("./examples/data/winds.grib", engine='cfgrib')
-        self.array = array
-        options= {"longitude":{"Cyclic":[0,360.]}}
-        self.xarraydatacube = XArrayDatacube(array)
-        self.slicer = HullSlicer()
-        self.API = Polytope(datacube=array, engine=self.slicer, options = options)
-
-    
-    def test_slice_shipping_route(self):
-        gdal.SetConfigOption('SHAPE_RESTORE_SHX', 'YES')
-
-        shapefile = gpd.read_file("./examples/data/EMODnet_HA_WindFarms_pg_20220324.shp")
-        polygons = []
-        for i in range(306):
-            country = shapefile.iloc[i]
-            multi_polygon = shape(country["geometry"])
-            if multi_polygon.geom_type == 'MultiPolygon':
-                true_polygons = list(multi_polygon.geoms)
-                for true_polygon in true_polygons:
-                    polygons.append(true_polygon)
-            else:
-                polygons.append(multi_polygon)
-        polygons_list = []
-        
-        # Now create a list of x,y points for each polygon
-
-        for polygon in polygons:
-             xx, yy = polygon.exterior.coords.xy
-             polygon_points = [list(a) for a in zip(xx,yy)]
-             polygons_list.append(polygon_points)
-
-        # Then do union of the polygon objects and cut using the slicer
-        print("done")
-        poly = []
-        for points in polygons_list:
-            polygon = Polygon(["longitude", "latitude"], points)
-            poly.append(polygon)
-        print(len(poly))
-        request_obj = poly[0]
-
-        for obj in poly:
-            request_obj = Union(["longitude", "latitude"], request_obj, obj)
-    
-        request = Request(request_obj, 
-                Select("step", [np.timedelta64(0, "ns")]))
-
-
-        # Extract the values of the long and lat from the tree
-        result = self.API.retrieve(request)
-        country_points_plotting = []
-        lats = []
-        longs = []
-        parameter_values = []
-        winds_u = []
-        winds_v = []
-        array = self.array
-        for i in range(len(result.leaves)):
-                cubepath = result.leaves[i].flatten()
-                lat = cubepath["latitude"]
-                long = cubepath["longitude"]
-                if long<-180:
-                    long = long +360
-                if long>180:
-                    long = long -360
-                lats.append(lat)
-                longs.append(long)
-            
-                u10_idx = result.leaves[i].result["u10"]
-                wind_u = u10_idx
-                v10_idx = result.leaves[i].result["v10"]
-                wind_v = v10_idx
-                winds_u.append(wind_u)
-                winds_v.append(wind_v)
-                parameter_values.append(math.sqrt(wind_u**2 + wind_v**2))
-
-        parameter_values = np.array(parameter_values)
-        # Plot this last array according to different colors for the result on a world map
-        worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-        fig, ax = plt.subplots(figsize=(12, 6))
-        worldmap.plot(color="darkgrey", ax=ax)
-        ax.scatter(longs, lats, s=8, c=parameter_values, cmap="viridis")
-        norm = mpl.colors.Normalize(vmin=min(parameter_values),vmax=max(parameter_values))
-
-        sm = plt.cm.ScalarMappable(cmap="viridis", norm=norm)
-        sm.set_array([])
-        plt.colorbar(sm, label="Wind Speed")
-
-        plt.show()
-        
