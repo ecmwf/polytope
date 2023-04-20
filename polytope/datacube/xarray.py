192ae2d4c3d33a719b8bd5408a8a12b412ac362c
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2023-04-20 16:04:16 +0200

    smaller functions in xarray and hullslicer

--- polytope/datacube/xarray.py
+++ polytope/datacube/xarray.py
@@ -29,8 +29,29 @@ _mappings = {
 
 
 class XArrayDatacube(Datacube):
-
     """Xarray arrays are labelled, axes can be defined as strings or integers (e.g. "time" or 0)."""
+
+    def _set_mapper(self, values, name):
+        if values.dtype.type not in _mappings:
+            raise ValueError(f"Could not create a mapper for index type {values.dtype.type} for axis {name}")
+        if name in self.options.keys():
+            # The options argument here is supposed to be a nested dictionary
+            # like {"latitude":{"Cyclic":range}, ...}
+            # TODO: would it be faster if instead we just add an option when it's cyclic and then evaluate to
+            # true or false? Maybe there is a better/faster way of accessing options
+            if "Cyclic" in self.options[name].keys():
+                value_type = values.dtype.type
+                axes_type_str = type(_mappings[value_type]).__name__
+                axes_type_str += "Cyclic"
+                cyclic_axis_type = deepcopy(
+                    getattr(sys.modules["polytope.datacube.datacube_axis"], axes_type_str)()
+                )
+                self.mappers[name] = cyclic_axis_type
+                self.mappers[name].name = name
+                self.mappers[name].range = self.options[name]["Cyclic"]
+        else:
+            self.mappers[name] = deepcopy(_mappings[values.dtype.type])
+            self.mappers[name].name = name
 
     def __init__(self, dataarray: xr.DataArray, options={}):
         self.options = options
@@ -38,30 +59,9 @@ class XArrayDatacube(Datacube):
         for name, values in dataarray.coords.variables.items():
             if name in dataarray.dims:
                 dataarray = dataarray.sortby(name)
-                if values.dtype.type not in _mappings:
-                    raise ValueError(f"Could not create a mapper for index type {values.dtype.type} for axis {name}")
-                if name in self.options.keys():
-                    # The options argument here is supposed to be a nested dictionary
-                    # like {"latitude":{"Cyclic":range}, ...}
-                    # TODO: would it be faster if instead we just add an option when it's cyclic and then evaluate to
-                    # true or false? Maybe there is a better/faster way of accessing options
-                    if "Cyclic" in self.options[name].keys():
-                        value_type = values.dtype.type
-                        axes_type_str = type(_mappings[value_type]).__name__
-                        axes_type_str += "Cyclic"
-                        cyclic_axis_type = deepcopy(
-                            getattr(sys.modules["polytope.datacube.datacube_axis"], axes_type_str)()
-                        )
-                        self.mappers[name] = cyclic_axis_type
-                        self.mappers[name].name = name
-                        self.mappers[name].range = self.options[name]["Cyclic"]
-                else:
-                    self.mappers[name] = deepcopy(_mappings[values.dtype.type])
-                    self.mappers[name].name = name
-
+                self._set_mapper(values, name)
             else:  # drop non-necessary coordinates which we don't slice on
                 dataarray = dataarray.reset_coords(names=name, drop=True)
-
         self.dataarray = dataarray
 
     def get(self, requests: DatacubeRequestTree):
@@ -86,29 +86,8 @@ class XArrayDatacube(Datacube):
             path[key] = self.mappers[key].remap_val_to_axis_range(value)
         return path
 
-    def get_indices(self, path: DatacubePath, axis, lower, upper):
-        path = self.remap_path(path)
-        # Open a view on the subset identified by the path
-        subarray = self.dataarray.sel(path, method="nearest")
-
-        # Get the indexes of the axis we want to query
-        # XArray does not support branching, so no need to use label, we just take the next axis
-        # TODO: should assert that the label == next axis
-
-        indexes = next(iter(subarray.xindexes.values())).to_pandas_index()
-
-        # Here, we do a cyclic remapping so we look up on the right existing values in the cyclic range on the datacube
+    def _look_up_datacube(self, search_ranges, search_ranges_offset, indexes, axis):
         idx_between = []
-        search_ranges = axis.remap([lower, upper])
-        original_search_ranges = axis.to_intervals([lower, upper])
-
-        # Find the offsets for each interval in the requested range, which we will need later
-        search_ranges_offset = []
-        for r in original_search_ranges:
-            offset = axis.offset(r)
-            search_ranges_offset.append(offset)
-
-        # Look up the values in the datacube for each cyclic interval range
         for i in range(len(search_ranges)):
             r = search_ranges[i]
             offset = search_ranges_offset[i]
@@ -130,6 +109,31 @@ class XArrayDatacube(Datacube):
                     indexes_between[j] = round(indexes_between[j] + offset, int(-math.log10(axis.tol)))
 
                 idx_between.append(indexes_between[j])
+        return idx_between
+
+    def get_indices(self, path: DatacubePath, axis, lower, upper):
+        path = self.remap_path(path)
+        # Open a view on the subset identified by the path
+        subarray = self.dataarray.sel(path, method="nearest")
+
+        # Get the indexes of the axis we want to query
+        # XArray does not support branching, so no need to use label, we just take the next axis
+        # TODO: should assert that the label == next axis
+
+        indexes = next(iter(subarray.xindexes.values())).to_pandas_index()
+
+        # Here, we do a cyclic remapping so we look up on the right existing values in the cyclic range on the datacube
+        search_ranges = axis.remap([lower, upper])
+        original_search_ranges = axis.to_intervals([lower, upper])
+
+        # Find the offsets for each interval in the requested range, which we will need later
+        search_ranges_offset = []
+        for r in original_search_ranges:
+            offset = axis.offset(r)
+            search_ranges_offset.append(offset)
+
+        # Look up the values in the datacube for each cyclic interval range
+        idx_between = self._look_up_datacube(search_ranges, search_ranges_offset, indexes, axis)
 
         # Remove duplicates even if difference of the order of the axis tolerance
         if offset is not None:
