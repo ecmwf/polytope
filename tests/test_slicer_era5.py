48a85f54009b28a61bd390cca75ed470e16a9716
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2023-05-09 14:56:56 +0200

    make work with index files for grib

--- tests/test_slicer_era5.py
+++ tests/test_slicer_era5.py
@@ -8,8 +8,11 @@ from polytope.shapes import Box, Select
 
 class TestSlicingEra5Data:
     def setup_method(self, method):
-        array = xr.open_dataset("./tests/data/era5-levels-members.grib", engine="cfgrib",
-                                backend_kwargs={'filter_by_keys': {'drop': ['paramId']}})
+        array = xr.open_dataset(
+            "./tests/data/era5-levels-members.grib",
+            engine="cfgrib",
+            backend_kwargs={"filter_by_keys": {"drop": ["paramId"]}},
+        )
         self.xarraydatacube = XArrayDatacube(array)
         self.slicer = HullSlicer()
         self.API = Polytope(datacube=array, engine=self.slicer)
