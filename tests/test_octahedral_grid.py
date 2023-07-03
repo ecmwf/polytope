42bbd6ce7c5d2c9d4f1cede1faacf4d5cc50bb63
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2023-07-03 10:09:21 +0100

    make better tests

--- tests/test_octahedral_grid.py
+++ tests/test_octahedral_grid.py
@@ -1,10 +1,5 @@
-import geopandas as gpd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
 from earthkit import data
 from eccodes import codes_grib_find_nearest, codes_grib_new_from_file
-from matplotlib import markers
 
 from polytope.datacube.octahedral_xarray import OctahedralXArrayDatacube
 from polytope.engine.hullslicer import HullSlicer
@@ -12,72 +7,61 @@ from polytope.polytope import Polytope, Request
 from polytope.shapes import Box
 
 
-def find_nearest_latlon(grib_file, target_lat, target_lon):
-    # Open the GRIB file
-    f = open(grib_file)
+class TestOctahedralGrid:
+    def setup_method(self, method):
+        ds = data.from_source("file", "./foo.grib")
+        latlon_array = ds.to_xarray().isel(step=0).isel(number=0).isel(surface=0).isel(time=0)
+        latlon_array = latlon_array.t2m
+        self.xarraydatacube = OctahedralXArrayDatacube(latlon_array)
+        self.slicer = HullSlicer()
+        self.API = Polytope(datacube=latlon_array, engine=self.slicer)
 
-    # Load the GRIB messages from the file
-    messages = []
-    while True:
-        message = codes_grib_new_from_file(f)
-        if message is None:
-            break
-        messages.append(message)
+    def find_nearest_latlon(self, grib_file, target_lat, target_lon):
+        # Open the GRIB file
+        f = open(grib_file)
 
-    # Find the nearest grid points
-    nearest_points = []
-    for message in messages:
-        nearest_index = codes_grib_find_nearest(message, target_lat, target_lon)
-        nearest_points.append(nearest_index)
+        # Load the GRIB messages from the file
+        messages = []
+        while True:
+            message = codes_grib_new_from_file(f)
+            if message is None:
+                break
+            messages.append(message)
 
-    # Close the GRIB file
-    f.close()
+        # Find the nearest grid points
+        nearest_points = []
+        for message in messages:
+            nearest_index = codes_grib_find_nearest(message, target_lat, target_lon)
+            nearest_points.append(nearest_index)
 
-    return nearest_points
+        # Close the GRIB file
+        f.close()
 
+        return nearest_points
 
-ds = data.from_source("file", "./foo.grib")
-latlon_array = ds.to_xarray().isel(step=0).isel(number=0).isel(surface=0).isel(time=0)
-latlon_array = latlon_array.t2m
-nearest_points = find_nearest_latlon("./foo.grib", 0, 0)
+    def test_octahedral_grid(self):
+        request = Request(Box(["latitude", "longitude"], [0, 0], [0.5, 0.5]))
+        result = self.API.retrieve(request)
+        assert len(result.leaves) == 56
 
-latlon_xarray_datacube = OctahedralXArrayDatacube(latlon_array)
-
-slicer = HullSlicer()
-API = Polytope(datacube=latlon_array, engine=slicer)
-
-request = Request(Box(["latitude", "longitude"], [0, 0], [0.5, 0.5]))
-result = API.retrieve(request)
-result.pprint()
-
-lats = []
-longs = []
-eccodes_lats = []
-eccodes_longs = []
-parameter_values = []
-for i in range(len(result.leaves)):
-    cubepath = result.leaves[i].flatten()
-    lat = cubepath["latitude"]
-    long = cubepath["longitude"]
-    lats.append(lat)
-    longs.append(long)
-    nearest_points = find_nearest_latlon("./foo.grib", lat, long)
-    eccodes_lats.append(nearest_points[0][0]["lat"])
-    eccodes_longs.append(nearest_points[0][0]["lon"])
-    t = result.leaves[i].result[1]
-    parameter_values.append(t)
-
-
-parameter_values = np.array(parameter_values)
-# Plot this last array according to different colors for the result on a world map
-worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-fig, ax = plt.subplots(figsize=(12, 6))
-worldmap.plot(color="darkgrey", ax=ax)
-marker = markers.MarkerStyle(marker="s")
-ax.scatter(eccodes_longs, eccodes_lats, s=12, c="red", marker=marker, facecolors="none")
-ax.scatter(longs, lats, s=4, c=parameter_values, cmap="viridis")
-norm = mpl.colors.Normalize(vmin=min(parameter_values), vmax=max(parameter_values))
-sm = plt.cm.ScalarMappable(cmap="viridis", norm=norm)
-sm.set_array([])
-plt.colorbar(sm, label="Wind Speed")
-plt.show()
+        lats = []
+        lons = []
+        eccodes_lats = []
+        eccodes_lons = []
+        tol = 1e-3
+        for i in range(len(result.leaves)):
+            cubepath = result.leaves[i].flatten()
+            lat = cubepath["latitude"]
+            lon = cubepath["longitude"]
+            lats.append(lat)
+            lons.append(lon)
+            nearest_points = self.find_nearest_latlon("./foo.grib", lat, lon)
+            eccodes_lat = nearest_points[0][0]["lat"]
+            eccodes_lon = nearest_points[0][0]["lon"]
+            eccodes_lats.append(eccodes_lat)
+            eccodes_lons.append(eccodes_lon)
+            assert eccodes_lat - tol <= lat
+            assert lat <= eccodes_lat + tol
+            assert eccodes_lon - tol <= lon
+            assert lon <= eccodes_lon + tol
+        assert len(eccodes_lats) == 56
