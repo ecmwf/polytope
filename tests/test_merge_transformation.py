0e5833adeba48cfc7d1f5fedfe1de4553d5dc698
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2023-08-04 17:24:00 +0200

    add test

---
+++ tests/test_merge_transformation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import xarray as xr
+
+from polytope.datacube.backends.xarray import XArrayDatacube
+from polytope.engine.hullslicer import HullSlicer
+from polytope.polytope import Polytope, Request
+from polytope.shapes import Select
+
+
+class TestSlicing4DXarrayDatacube:
+    def setup_method(self, method):
+        # Create a dataarray with 4 labelled axes using different index types
+        array = xr.DataArray(
+            np.random.randn(1, 1),
+            dims=("date", "time"),
+            coords={
+                "date": ["2000-01-01"],
+                "time": ["06:00"],
+            },
+        )
+        options = {"date": {"transformation": {"merge": {"with": "time", "linkers": ["T", ":00"]}}}}
+        self.xarraydatacube = XArrayDatacube(array)
+        self.slicer = HullSlicer()
+        self.API = Polytope(datacube=array, engine=self.slicer, axis_options=options)
+
+    def test_merge_axis(self):
+        request = Request(Select("date", ["2000-01-01T06:00:00"]))
+        result = self.API.retrieve(request)
+        assert result.flatten()["date"] == "2000-01-01T06:00:00"
