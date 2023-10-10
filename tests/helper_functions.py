040f3df16a7faa5f4aa4fe7731723a966f6ec87a
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2023-10-10 11:12:56 +0100

    remove exception handling in helper functions

--- tests/helper_functions.py
+++ tests/helper_functions.py
@@ -15,7 +15,7 @@ def download_test_data(nexus_url, filename):
     if not os.path.exists(local_file_path):
         session = requests.Session()
         response = session.get(nexus_url)
-        response.raise_for_status()
+        # response.raise_for_status()
         # Save the downloaded data to the local file
         with open(local_file_path, "wb") as f:
             f.write(response.content)
