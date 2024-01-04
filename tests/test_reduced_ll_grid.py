8a6c9d36a0eed33603e2cab353c0d30eef3357f0
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2024-01-04 10:21:53 +0100

    make plot in test reduced ll grid

--- tests/test_reduced_ll_grid.py
+++ tests/test_reduced_ll_grid.py
@@ -1,3 +1,5 @@
+# import geopandas as gpd
+# import matplotlib.pyplot as plt
 import pandas as pd
 import pytest
 from eccodes import codes_grib_find_nearest, codes_grib_new_from_file
@@ -30,30 +32,18 @@ class TestReducedLatLonGrid:
         self.API = Polytope(datacube=self.fdbdatacube, engine=self.slicer, axis_options=self.options)
 
     def find_nearest_latlon(self, grib_file, target_lat, target_lon):
-        # Open the GRIB file
-        f = open(grib_file)
-
-        # Load the GRIB messages from the file
-        messages = []
-        while True:
-            message = codes_grib_new_from_file(f)
-            if message is None:
-                break
-            messages.append(message)
+        messages = grib_file
 
         # Find the nearest grid points
         nearest_points = []
-        for message in messages:
+        for message in [messages[0]]:
             nearest_index = codes_grib_find_nearest(message, target_lat, target_lon)
             nearest_points.append(nearest_index)
-
-        # Close the GRIB file
-        f.close()
 
         return nearest_points
 
     @pytest.mark.internet
-    @pytest.mark.skip(reason="can't install fdb branch on CI")
+    # @pytest.mark.skip(reason="can't install fdb branch on CI")
     def test_reduced_ll_grid(self):
         request = Request(
             Select("step", [1]),
@@ -76,20 +66,40 @@ class TestReducedLatLonGrid:
         lats = []
         lons = []
         eccodes_lats = []
+        eccodes_lons = []
         tol = 1e-8
+        f = open("./tests/data/wave.grib")
+        messages = []
+        while True:
+            message = codes_grib_new_from_file(f)
+            if message is None:
+                break
+            messages.append(message)
+
         for i in range(len(result.leaves)):
             cubepath = result.leaves[i].flatten()
             lat = cubepath["latitude"]
             lon = cubepath["longitude"]
             lats.append(lat)
             lons.append(lon)
-            nearest_points = self.find_nearest_latlon("./tests/data/wave.grib", lat, lon)
+            nearest_points = self.find_nearest_latlon(messages, lat, lon)
             eccodes_lat = nearest_points[0][0]["lat"]
             eccodes_lon = nearest_points[0][0]["lon"]
             eccodes_lats.append(eccodes_lat)
+            eccodes_lons.append(eccodes_lon)
             assert eccodes_lat - tol <= lat
             assert lat <= eccodes_lat + tol
             assert eccodes_lon - tol <= lon
             assert lon <= eccodes_lon + tol
+        f.close()
+
+        # worldmap = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+        # fig, ax = plt.subplots(figsize=(12, 6))
+        # worldmap.plot(color="darkgrey", ax=ax)
+
+        # plt.scatter(lons, lats, s=18, c="red", cmap="YlOrRd")
+        # plt.scatter(eccodes_lons, eccodes_lats, s=6, c="green")
+        # plt.colorbar(label="Temperature")
+        # plt.show()
 
         assert len(eccodes_lats) == 10
