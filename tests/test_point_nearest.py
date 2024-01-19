e5dfb8e331225025e202c9156b0e7e2836661c21
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2024-01-19 10:07:13 +0100

    make tests work

--- tests/test_point_nearest.py
+++ tests/test_point_nearest.py
@@ -38,7 +38,7 @@ class TestSlicingFDBDatacube:
             Point(["latitude", "longitude"], [[0.16, 0.176]], method="nearest"),
         )
         result = self.API.retrieve(request)
-        result.pprint()
+        # result.pprint()
         assert len(result.leaves) == 1
 
     @pytest.mark.fdb
@@ -56,5 +56,23 @@ class TestSlicingFDBDatacube:
             Point(["latitude", "longitude"], [[0.175746921078, 0.210608424337]], method="nearest"),
         )
         result = self.API.retrieve(request)
-        result.pprint()
+        # result.pprint()
         assert len(result.leaves) == 1
+
+    @pytest.mark.fdb
+    def test_fdb_datacube_true_point_2(self):
+        request = Request(
+            Select("step", [21, 22, 23]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20231102T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[0.035149384216, 0.0]], method="nearest"),
+        )
+        result = self.API.retrieve(request)
+        result.pprint()
+        assert len(result.leaves) == 3
