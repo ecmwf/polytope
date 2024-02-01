c666d0fe428afad3772488acd2dc55a5f94fd242
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2024-02-01 14:01:44 +0000

    ignore pygribjump test

--- tests/test_local_grid_cyclic.py
+++ tests/test_local_grid_cyclic.py
@@ -51,6 +51,7 @@ class TestSlicingFDBDatacube:
         assert result.leaves[0].flatten()["latitude"] == -20
         assert result.leaves[0].flatten()["longitude"] == -20
 
+    @pytest.mark.fdb
     def test_fdb_datacube_2(self):
         request = Request(
             Select("step", [0]),
