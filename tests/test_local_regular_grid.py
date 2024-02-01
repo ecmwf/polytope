19b490644f78198d65113cb8cc1a56f475302a76
Mathilde Leuridan <mathilde.leuridan@ecmwf.int>
2024-02-01 13:43:58 +0000

    test local grid with cyclicity

--- tests/test_local_regular_grid.py
+++ tests/test_local_regular_grid.py
@@ -23,7 +23,6 @@ class TestSlicingFDBDatacube:
             "date": {"merge": {"with": "time", "linkers": ["T", "00"]}},
             "step": {"type_change": "int"},
             "number": {"type_change": "int"},
-            "longitude": {"cyclic": [0, 360]},
         }
         self.config = {"class": "od", "expver": "0001", "levtype": "sfc", "stream": "oper"}
         self.fdbdatacube = FDBDatacube(self.config, axis_options=self.options)
@@ -50,3 +49,179 @@ class TestSlicingFDBDatacube:
         assert len(result.leaves) == 1
         assert result.leaves[0].flatten()["latitude"] == 0
         assert result.leaves[0].flatten()["longitude"] == 0
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[0.16, 61]], method="nearest"),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 1
+        assert result.leaves[0].flatten()["latitude"] == 0
+        assert result.leaves[0].flatten()["longitude"] == 60
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region_2(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[41, 1]], method="nearest"),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 1
+        assert result.leaves[0].flatten()["latitude"] == 40
+        assert result.leaves[0].flatten()["longitude"] == 1
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region_3(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[1, 61]]),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 1
+        assert result.is_root()
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region_4(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[41, 1]]),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 1
+        assert result.is_root()
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region_5(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[-41, 1]]),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 1
+        assert result.is_root()
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region_6(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[-30, -21]]),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 1
+        assert result.is_root()
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region_7(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[-41, 1]], method="nearest"),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 1
+        assert result.leaves[0].flatten()["latitude"] == -40
+        assert result.leaves[0].flatten()["longitude"] == 1
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region_8(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[-30, -21]], method="nearest"),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 1
+        assert result.leaves[0].flatten()["latitude"] == -30
+        assert result.leaves[0].flatten()["longitude"] == -20
+
+    @pytest.mark.fdb
+    def test_point_outside_local_region_9(self):
+        request = Request(
+            Select("step", [0]),
+            Select("levtype", ["sfc"]),
+            Select("date", [pd.Timestamp("20240129T000000")]),
+            Select("domain", ["g"]),
+            Select("expver", ["0001"]),
+            Select("param", ["167"]),
+            Select("class", ["od"]),
+            Select("stream", ["oper"]),
+            Select("type", ["fc"]),
+            Point(["latitude", "longitude"], [[-30, -21]], method="surrounding"),
+        )
+        result = self.API.retrieve(request)
+        result.pprint_2()
+        assert len(result.leaves) == 3
+        assert result.leaves[0].flatten()["latitude"] == -31
+        assert result.leaves[0].flatten()["longitude"] == -20
