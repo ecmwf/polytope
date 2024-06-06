07cdf3f6b8ca42bfee2dcbef3552b5a4a4557b12
mathleur <mathilde.leuridan@ecmwf.int>
2024-06-06 16:30:11 +0200

    add protobuf encoding tests and fix encoding

--- polytope/datacube/tree_encoding.py
+++ polytope/datacube/tree_encoding.py
@@ -2,10 +2,10 @@ from copy import deepcopy
 
 from . import index_tree_pb2 as pb2
 from .datacube_axis import IntDatacubeAxis
-from .index_tree_pb2 import IndexTree
+from .tensor_index_tree import TensorIndexTree
 
 
-def encode_tree(tree: IndexTree):
+def encode_tree(tree: TensorIndexTree):
     node = pb2.Node()
 
     node.axis = tree.axis.name
@@ -23,7 +23,7 @@ def encode_tree(tree: IndexTree):
     return node.SerializeToString()
 
 
-def encode_child(tree: IndexTree, child: IndexTree, node, result_size=[]):
+def encode_child(tree: TensorIndexTree, child: TensorIndexTree, node, result_size=[]):
     child_node = pb2.Node()
 
     # Add the result size to the final node
@@ -32,8 +32,8 @@ def encode_child(tree: IndexTree, child: IndexTree, node, result_size=[]):
     if len(child.children) == 0:
         # TODO: here, we need to find the last node which isn't hidden and add all of this to that one
         result_size.append(len(child.values))
-        result_size.append(len(child.indexes))
-        child_node.result_size.extend(result_size)
+        # result_size.append(len(child.indexes))
+        child_node.size_result.extend(result_size)
         child_node.indexes.extend(child.indexes)
 
     # TODO: need to add axis and children etc to the encoded node only if the tree node isn't hidden 
@@ -42,11 +42,11 @@ def encode_child(tree: IndexTree, child: IndexTree, node, result_size=[]):
     # NOTE: do we need this if we parse the tree before it has values?
     # TODO: not clear if child.value is a numpy array or a simple float...
     # TODO: not clear what happens if child.value is a np array since this is not a supported type by protobuf
-    if child.result is not None:
-        if isinstance(child.result, list):
-            child_node.result.extend(child.result)
-        else:
-            child_node.result.append(child.result)
+    # if child.result is not None:
+    #     if isinstance(child.result, list):
+    #         child_node.result.extend(child.result)
+    #     else:
+    #         child_node.result.append(child.result)
 
     # Assign the node value according to the type
     child_node.value.extend(child.values)
@@ -59,14 +59,15 @@ def encode_child(tree: IndexTree, child: IndexTree, node, result_size=[]):
         encode_child(child, c, child_node, new_result_size)
 
     # NOTE: we append the children once their branch has been completed until the leaf
-    node.children.append(child_node)
+    if not child.hidden:
+        node.children.append(child_node)
 
 
 def decode_tree(datacube, bytearray):
     node = pb2.Node()
     node.ParseFromString(bytearray)
 
-    tree = IndexTree()
+    tree = TensorIndexTree()
 
     if node.axis == "root":
         root = IntDatacubeAxis()
@@ -84,11 +85,11 @@ def decode_tree(datacube, bytearray):
 def decode_child(node, tree, datacube):
     if len(node.children) == 0:
         tree.result = node.result
-        tree.result_size = node.result_size
+        tree.result_size = node.size_result
         tree.indexes = node.indexes
     for child in node.children:
         child_axis = datacube._axes[child.axis]
         child_vals = tuple(child.value)
-        child_node = IndexTree(child_axis, child_vals)
+        child_node = TensorIndexTree(child_axis, child_vals)
         tree.add_child(child_node)
         decode_child(child, child_node, datacube)
