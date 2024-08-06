01ab13442e5fa63982db909eab2b1fc472661a41
mathleur <mathilde.leuridan@ecmwf.int>
2024-08-06 10:03:23 +0200

    fix comparing time values with floats for comparison

--- polytope/datacube/tensor_index_tree.py
+++ polytope/datacube/tensor_index_tree.py
@@ -81,8 +81,12 @@ class TensorIndexTree(object):
                     for i in range(len(other.values)):
                         other_val = other.values[i]
                         self_val = self.values[i]
-                        if abs(other_val - self_val) > 2 * max(other.axis.tol, self.axis.tol):
-                            return False
+                        if self.axis.can_round:
+                            if abs(other_val - self_val) > 2 * max(other.axis.tol, self.axis.tol):
+                                return False
+                        else:
+                            if other_val != self_val:
+                                return False
                     return True
 
     def __lt__(self, other):
