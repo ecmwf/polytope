f1a808ae1e39fdc16af933de8d3371ed2d9b8b1a
mathleur <mathilde.leuridan@ecmwf.int>
2025-01-28 15:28:52 +0100

    try to fix reduced_ll grid for res 3601

--- polytope_feature/datacube/transformations/datacube_mappers/mapper_types/reduced_ll.py
+++ polytope_feature/datacube/transformations/datacube_mappers/mapper_types/reduced_ll.py
@@ -25,12 +25,12 @@ class ReducedLatLonMapper(DatacubeMapper):
         start_lat = 90
         end_lat = -90
         if self._resolution == 3601:
-            start_lat = 89.9731
-            end_lat = -89.9731
+            start_lat = 89.973092
+            # end_lat = -89.9731
         # resolution = 180 / (self._resolution - 1)
         # vals = [round(90 - i * resolution, 12) for i in range(self._resolution)]
-        # resolution = 180 / (self._resolution - 1)
-        resolution = (start_lat - end_lat) / (self._resolution - 1)
+        resolution = 180 / (self._resolution - 1)
+        # resolution = (start_lat - end_lat) / (self._resolution - 1)
         vals = [round(start_lat - i * resolution, 12) for i in range(self._resolution)]
         return vals
 
