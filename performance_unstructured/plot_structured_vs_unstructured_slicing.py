64ddb6ec9087437311b9af9df76a26e0164bf9e7
mathleur <mathilde.leuridan@ecmwf.int>
2025-03-05 09:20:28 +0100

    plot only the polytope time without gj

--- performance_unstructured/plot_structured_vs_unstructured_slicing.py
+++ performance_unstructured/plot_structured_vs_unstructured_slicing.py
@@ -16,10 +16,18 @@ tot_retrieve_time_structured = [0.2735710144042969, 0.5628271102905273, 1.519225
 tot_retrieve_time_unstructured = [0.9918620586395264, 5.218957185745239, 33.4666748046875, 210.05072903633118]
 
 
+pure_poly_time_structured = [tot_retrieve_time_structured[i] - gj_retrieve_structured_time[i] for i in range(4)]
+pure_poly_time_unstructured = [tot_retrieve_time_unstructured[i] - gj_retrieve_unstructured_time[i] for i in range(4)]
+
 fig, ax = plt.subplots(1, 1)
-ax.plot(box_size, tot_retrieve_time_structured, color="b")
-ax.plot(box_size, tot_retrieve_time_unstructured, color="r")
-ax.plot(box_size, gj_retrieve_structured_time, color="b", linestyle="--")
-ax.plot(box_size, gj_retrieve_unstructured_time, color="r", linestyle="--")
-ax.set_yscale("log")
+
+# ax.plot(box_size, tot_retrieve_time_structured, color="b")
+# ax.plot(box_size, tot_retrieve_time_unstructured, color="r")
+# ax.plot(box_size, gj_retrieve_structured_time, color="b", linestyle="--")
+# ax.plot(box_size, gj_retrieve_unstructured_time, color="r", linestyle="--")
+# ax.set_yscale("log")
+
+ax.plot(box_size, pure_poly_time_structured, color="b")
+ax.plot(box_size, pure_poly_time_unstructured, color="r")
+
 plt.show()
