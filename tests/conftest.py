f4cf1964459eb3ec595a7097baf9e952c166e03d
mathleur <mathilde.leuridan@ecmwf.int>
2025-04-14 11:02:06 +0200

    add pytest fixtures to add fdb data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
+from requests.exceptions import HTTPError
 import pytest
 
 import pathlib
+import yaml
+import os
+import shutil
+
+import tempfile
+import requests
+from .helper_functions import HTTPError
+import pyfdb
 
 
 @pytest.fixture
@@ -10,7 +19,7 @@ def load_fdb_data_from_nexus():
 
 
 @pytest.fixture
-def fdb_data_path(request) -> pathlib.Path:
+def fdb_path(request) -> pathlib.Path:
     """
     Provides path to test data at '<src-root>/tests/fdb_data'
     """
@@ -20,6 +29,86 @@ def fdb_data_path(request) -> pathlib.Path:
 
 
 @pytest.fixture(scope="session")
-def fdb_store_operational(fdb_data_path) -> pathlib.Path:
+def fdb_store_operational_setup(fdb_path, tmp_path, downloaded_data_test_files) -> pathlib.Path:
     # TODO: once we have the test data in the fdb_data folder, and we have a path to it, load it all in an fdb
-    pass
+
+    # Set up FDB config and schemas
+    db_store_path = tmp_path / "db_store"
+    db_store_path.mkdir(exist_ok=True)
+    schema_path = tmp_path / "schema"
+    config = dict(
+        type="local",
+        engine="toc",
+        schema=str(schema_path),
+        spaces=[
+            dict(
+                handler="Default",
+                roots=[
+                    {"path": str(db_store_path)},
+                ],
+            )
+        ],
+    )
+    config_path = tmp_path / "config.yaml"
+    config_path.write_text(yaml.dump(config))
+    shutil.copy(fdb_path / "schema", schema_path)
+    os.environ["FDB5_CONFIG_FILE"] = str(config_path)
+
+    # TODO: download data from the internet
+    # grib_files = data_path.glob("*.grib")
+    # grib_files = []
+    # for file_name in downloaded_data_test_files:
+    #     # grib_files = [data_path / "synth11.grib"]
+    #     grib_files.append(file_name)
+    fdb = pyfdb.FDB()
+    # for f in grib_files:
+    for f in downloaded_data_test_files:
+        fdb.archive(f.read_bytes())
+    return tmp_path
+
+
+@pytest.fixture(scope="session")
+def shared_temp_data_dir(tmp_path_factory):
+    # This creates a unique temp dir for the whole test session
+    temp_dir = tmp_path_factory.mktemp("shared_fdb_data")
+    return temp_dir
+
+
+@pytest.fixture(scope="session")
+def downloaded_data_test_files(shared_temp_data_dir):
+    files_to_download = [
+        # ("https://example.com/file1.csv", "file1.csv"),
+        # ("https://example.com/file2.csv", "file2.csv"),
+        # ("https://example.com/file3.csv", "file3.csv"),
+        ("https://get.ecmwf.int/test-data/polytope/test-data/foo.grib", "foo.grib")
+    ]
+
+    downloaded_paths = []
+
+    for (url, filename) in files_to_download:
+        path = shared_temp_data_dir / filename
+        if not path.exists():
+            response = requests.get(url)
+            if response.status_code != 200:
+                raise HTTPError(f"HTTP {response.status_code} - Failed to download data.")
+            # path.write_bytes(response.content)
+            with open(path, "wb") as f:
+                f.write(response.content)
+        downloaded_paths.append(path)
+
+    return downloaded_paths
+
+
+# @pytest.fixture(scope="session")
+# def downloaded_test_file(shared_temp_data_dir, nexus_url, filename):
+
+#     local_file_path = shared_temp_data_dir / filename
+
+#     if not os.path.exists(local_file_path):
+#         response = requests.get(nexus_url)
+#         if response.status_code != 200:
+#             raise HTTPError(f"HTTP {response.status_code} - Failed to download data.")
+#         with open(local_file_path, "wb") as f:
+#             f.write(response.content)
+
+#     yield local_file_path
