147a75c40814109b705f71fbae3124f7b22e0c72
oisin-m <oisinmmm@gmail.com>
2025-05-14 10:57:18 +0200

    add prints

--- setup.py
+++ setup.py
@@ -6,10 +6,13 @@ from setuptools_rust import RustExtension
 use_rust = int(os.environ.get("USE_RUST", "-1"))
 
 if use_rust == 0:  # pure python
+    print("Building pure Python version.")
     rust_extensions = []
 elif use_rust == 1:  # rust extension
+    print("Building rust bindings version.")
     rust_extensions = [RustExtension("quadtree", "polytope_feature/datacube/quadtree/Cargo.toml")]
 else:  # (default) try rust extension, if fail fallback to python
+    print("Trying to build with rust bindings, and if failing reverting to pure Python.")
     rust_extensions = [RustExtension("quadtree", "polytope_feature/datacube/quadtree/Cargo.toml", optional=True)]
 
 setup(rust_extensions=rust_extensions)
