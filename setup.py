b9ebc73ddf97d62f44613b5461f3a9a3da502856
mathleur <mathilde.leuridan@ecmwf.int>
2025-06-27 11:28:12 +0200

    try to force rust build

--- setup.py
+++ setup.py
@@ -10,11 +10,28 @@ if use_rust == 0:  # pure python
     rust_extensions = []
 elif use_rust == 1:  # rust extension
     print("Building rust bindings version.")
-    rust_extensions = [RustExtension("polytope_feature.polytope_rs", "polytope_feature/datacube/quadtree/Cargo.toml")]
+    # rust_extensions = [RustExtension("polytope_feature.polytope_rs", "polytope_feature/datacube/quadtree/Cargo.toml")]
+    rust_extensions = [RustExtension(
+        "polytope_feature.polytope_rs",
+        path="polytope_feature/datacube/quadtree/Cargo.toml",
+        binding="pyo3",
+        debug=False,
+        # Optional: args to control the output
+        args=["--manifest-path=polytope_feature/datacube/quadtree/Cargo.toml"]
+    )]
 else:  # (default) try rust extension, if fail fallback to python
     print("Building with rust bindings, and if failing reverting to pure Python.")
-    rust_extensions = [
-        RustExtension("polytope_feature.polytope_rs", "polytope_feature/datacube/quadtree/Cargo.toml", optional=True)
-    ]
+    # rust_extensions = [
+    #     RustExtension("polytope_feature.polytope_rs", "polytope_feature/datacube/quadtree/Cargo.toml", optional=True)
+    # ]
+    rust_extensions = [RustExtension(
+        "polytope_feature.polytope_rs",
+        path="polytope_feature/datacube/quadtree/Cargo.toml",
+        binding="pyo3",
+        debug=False,
+        # Optional: args to control the output
+        args=["--manifest-path=polytope_feature/datacube/quadtree/Cargo.toml"],
+        optional=True,
+    )]
 
 setup(rust_extensions=rust_extensions)
