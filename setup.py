23917aec36d814b7919d762238e824104b877354
mathleur <mathilde.leuridan@ecmwf.int>
2025-06-27 11:20:22 +0200

    readd setup.py and pyproject.toml as before

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup
+from setuptools_rust import RustExtension
+
+use_rust = int(os.environ.get("USE_RUST", "-1"))
+
+if use_rust == 0:  # pure python
+    print("Building pure Python version.")
+    rust_extensions = []
+elif use_rust == 1:  # rust extension
+    print("Building rust bindings version.")
+    rust_extensions = [RustExtension("polytope_feature.polytope_rs", "polytope_feature/datacube/quadtree/Cargo.toml")]
+else:  # (default) try rust extension, if fail fallback to python
+    print("Building with rust bindings, and if failing reverting to pure Python.")
+    rust_extensions = [
+        RustExtension("polytope_feature.polytope_rs", "polytope_feature/datacube/quadtree/Cargo.toml", optional=True)
+    ]
+
+setup(rust_extensions=rust_extensions)
