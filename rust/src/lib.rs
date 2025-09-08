74499837ee9d3c4a643604e3321e6cd4b86b2f6f
mathleur <mathilde.leuridan@ecmwf.int>
2025-09-08 14:10:48 +0200

    reshuffle files

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -12,6 +12,13 @@ use crate::healpix_nested::{axes_idx_to_healpix_idx_batch, ring_to_nested_batche
 pub mod octahedral;
 use crate::octahedral::{unmap_octahedral, first_axis_vals_octahedral};
 
+pub mod quadtree_mod;
+pub mod slicing_tools;
+
+pub mod point_in_polygon;
+
+use crate::point_in_polygon::{extract_point_in_poly, extract_point_in_poly_bbox};
+
 #[pymodule]
 fn polytope_rs(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(get_latlons_sphere, m)?)?;
@@ -23,5 +30,10 @@ fn polytope_rs(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(unmap, m)?)?;
     m.add_function(wrap_pyfunction!(first_axis_vals_octahedral, m)?)?;
     m.add_function(wrap_pyfunction!(unmap_octahedral, m)?)?;
+    m.add_class::<quadtree_mod::QuadTree>()?;
+    m.add_class::<quadtree_mod::QuadTreeNode>()?;
+    m.add_function(wrap_pyfunction!(extract_point_in_poly, m)?)?;
+    m.add_function(wrap_pyfunction!(extract_point_in_poly_bbox, m)?)?;
     Ok(())
 }
+
