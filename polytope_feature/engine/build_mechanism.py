103f9fffa0abcf8a5233fb059fa6e8ae78925400
mathleur <mathilde.leuridan@ecmwf.int>
2025-11-13 17:07:45 +0100

    WIP: separate slicer and build mechanism in engine

---
+++ polytope_feature/engine/build_mechanism.py
@@ -0,0 +1,7 @@
+# TODO: apart from the slicer, we now differentiate between diff build mechanisms
+# We first have the option to build the tree more breadth first like in the hullslicer
+# And we have the second option to build more depth first like in the qubed slicer
+# NOTE: in particular, here we differentiate first between different return types as well
+
+
+# TODO: NOTE each engine will have only one build mechanism, but we can have different slicers on different axes
