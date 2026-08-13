2b3a79f41632d47a3d0b7f7219fc6ae9663bb223
Lasse Meinen <lasse.meinen@meteoswiss.ch>
2026-08-13 15:20:54 +0200

    moved unit tests that don't depend on FDB to separate class

--- tests/test_quad_tree.py
+++ tests/test_quad_tree.py
@@ -19,73 +19,6 @@ class TestQuadTreeSlicer:
         self.fdbdatacube = gj.GribJump()
 
     @pytest.mark.fdb
-    def test_quad_tree_query_polygon(self):
-        points = [[10, 10], [80, 10], [-5, 5], [5, 20], [5, 10], [50, 10]]
-        slicer = QuadTreeSlicer(points)
-        polytope = Box(["lat", "lon"], [1, 1], [20, 30]).polytope()[0]
-        results = query_polygon(points, slicer.quad_tree, 0, polytope)
-        assert len(results) == 3
-        assert (10, 10) in [slicer.points[node] for node in results]
-        assert (5, 10) in [slicer.points[node] for node in results]
-        assert (5, 20) in [slicer.points[node] for node in results]
-        points = [
-            [10, 10],
-            [80, 10],
-            [-5, 5],
-            [5, 50],
-            [5, 10],
-            [50, 10],
-            [2, 10],
-            [15, 15],
-        ]
-        slicer = QuadTreeSlicer(points)
-        polytope = ConvexPolytope(["lat", "lon"], [[-10, 1], [20, 1], [5, 20]])
-        results = query_polygon(points, slicer.quad_tree, 0, polytope)
-        assert len(results) == 4
-        assert (-5, 5) in [slicer.points[node] for node in results]
-        assert (5, 10) in [slicer.points[node] for node in results]
-        assert (10, 10) in [slicer.points[node] for node in results]
-        assert (2, 10) in [slicer.points[node] for node in results]
-
-    @pytest.mark.fdb
-    def test_slice_in_two_vertically(self):
-        polytope = Box(["lat", "lon"], [0, 0], [2, 2]).polytope()[0]
-        lower, upper = slice_in_two(polytope, 1, 0)
-        assert lower.points == [[0, 0], [1.0, 0.0], [1.0, 2.0], [0, 2]]
-        assert upper.points == [[1.0, 0.0], [2, 0], [2, 2], [1.0, 2.0]]
-
-    @pytest.mark.fdb
-    def test_slice_in_two_horizontally(self):
-        polytope = Box(["lat", "lon"], [0, 0], [2, 2]).polytope()[0]
-        lower, upper = slice_in_two(polytope, 1, 1)
-        assert lower.points == [[0, 0], [2, 0], [2.0, 1.0], [0.0, 1.0]]
-        assert upper.points == [[2, 2], [0, 2], [0.0, 1.0], [2.0, 1.0]]
-
-    @pytest.mark.fdb
-    def test_quad_node_is_contained_in_box(self):
-        node = QuadNode([1, 1], 0)
-        polytope = Box(["lat", "lon"], [0, 0], [2, 2]).polytope()[0]
-        assert node.is_contained_in(polytope)
-        second_node = QuadNode([3, 3], 0)
-        assert not second_node.is_contained_in(polytope)
-        third_node = QuadNode([1, 0], 0)
-        assert third_node.is_contained_in(polytope)
-
-    @pytest.mark.fdb
-    def test_quad_node_is_contained_in_triangle(self):
-        node = QuadNode([1, 1], 0)
-        polytope = ConvexPolytope(["lat", "lon"], [[0, 0], [1, 1], [2, 0]])
-        assert node.is_contained_in(polytope)
-        node = QuadNode([1, 0.5], 0)
-        assert node.is_contained_in(polytope)
-        second_node = QuadNode([3, 3], 0)
-        assert not second_node.is_contained_in(polytope)
-        third_node = QuadNode([1, 0], 0)
-        assert third_node.is_contained_in(polytope)
-        third_node = QuadNode([0.1, 0.5], 0)
-        assert not third_node.is_contained_in(polytope)
-
-    @pytest.mark.fdb
     def test_quad_tree_slicer_extract(self):
         points = [[10, 10], [80, 10], [-5, 5], [5, 20], [5, 10], [50, 10]]
         polytope = Box(["latitude", "longitude"], [1, 1], [20, 30]).polytope()[0]
@@ -242,6 +175,69 @@ class TestQuadTreeSlicer:
 
 
 class TestQuadTreeSlicerWithoutFdb:
+    @pytest.mark.skipif(not use_rust, reason="quadtree_additional_operations targets the Rust QuadTree API")
+    def test_quad_tree_query_polygon(self):
+        points = [[10, 10], [80, 10], [-5, 5], [5, 20], [5, 10], [50, 10]]
+        slicer = QuadTreeSlicer(points)
+        polytope = Box(["lat", "lon"], [1, 1], [20, 30]).polytope()[0]
+        results = query_polygon(points, slicer.quad_tree, 0, polytope)
+        assert len(results) == 3
+        assert (10, 10) in [slicer.points[node] for node in results]
+        assert (5, 10) in [slicer.points[node] for node in results]
+        assert (5, 20) in [slicer.points[node] for node in results]
+        points = [
+            [10, 10],
+            [80, 10],
+            [-5, 5],
+            [5, 50],
+            [5, 10],
+            [50, 10],
+            [2, 10],
+            [15, 15],
+        ]
+        slicer = QuadTreeSlicer(points)
+        polytope = ConvexPolytope(["lat", "lon"], [[-10, 1], [20, 1], [5, 20]])
+        results = query_polygon(points, slicer.quad_tree, 0, polytope)
+        assert len(results) == 4
+        assert (-5, 5) in [slicer.points[node] for node in results]
+        assert (5, 10) in [slicer.points[node] for node in results]
+        assert (10, 10) in [slicer.points[node] for node in results]
+        assert (2, 10) in [slicer.points[node] for node in results]
+
+    def test_slice_in_two_vertically(self):
+        polytope = Box(["lat", "lon"], [0, 0], [2, 2]).polytope()[0]
+        lower, upper = slice_in_two(polytope, 1, 0)
+        assert lower.points == [[0, 0], [1.0, 0.0], [1.0, 2.0], [0, 2]]
+        assert upper.points == [[1.0, 0.0], [2, 0], [2, 2], [1.0, 2.0]]
+
+    def test_slice_in_two_horizontally(self):
+        polytope = Box(["lat", "lon"], [0, 0], [2, 2]).polytope()[0]
+        lower, upper = slice_in_two(polytope, 1, 1)
+        assert lower.points == [[0, 0], [2, 0], [2.0, 1.0], [0.0, 1.0]]
+        assert upper.points == [[2, 2], [0, 2], [0.0, 1.0], [2.0, 1.0]]
+
+    def test_quad_node_is_contained_in_box(self):
+        node = QuadNode([1, 1], 0)
+        polytope = Box(["lat", "lon"], [0, 0], [2, 2]).polytope()[0]
+        assert node.is_contained_in(polytope)
+        second_node = QuadNode([3, 3], 0)
+        assert not second_node.is_contained_in(polytope)
+        third_node = QuadNode([1, 0], 0)
+        assert third_node.is_contained_in(polytope)
+
+    def test_quad_node_is_contained_in_triangle(self):
+        node = QuadNode([1, 1], 0)
+        polytope = ConvexPolytope(["lat", "lon"], [[0, 0], [1, 1], [2, 0]])
+        assert node.is_contained_in(polytope)
+        node = QuadNode([1, 0.5], 0)
+        assert node.is_contained_in(polytope)
+        second_node = QuadNode([3, 3], 0)
+        assert not second_node.is_contained_in(polytope)
+        third_node = QuadNode([1, 0], 0)
+        assert third_node.is_contained_in(polytope)
+        third_node = QuadNode([0.1, 0.5], 0)
+        assert not third_node.is_contained_in(polytope)
+
     def test_extract_single_longitude_first(self):
         grid = [(4, 1), (1, 4)]  # quadtree constructor assumes points are lat/lon
         slicer = QuadTreeSlicer(grid)
